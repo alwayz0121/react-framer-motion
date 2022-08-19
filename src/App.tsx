be4990d2739d2bab4e79302f178850c40b9ce732
Gyeong a Kim <alwayz0121@gmail.com>
2022-08-19 14:57:40 +0900

    #7.5 Drag motion

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
@@ -20,13 +20,20 @@ const Box = styled(motion.div)`
 const boxVariants = {
   hover: { scale: 1.5, rotate: 90 },
   click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
 };
 
-//마우스를 올리거나(whileHover), 클릭시(whileTap) 모양을 변경
+//애니메이션 효과를 주면서 색상 변경시 : 색을 숫자 값으로 줘야 한다.
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click" />
+      <Box
+        drag
+        variants={boxVariants}
+        whileHover="hover"
+        whileDrag="drag"
+        whileTap="click"
+      />
     </Wrapper>
   );
 }
