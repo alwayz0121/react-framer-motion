9552e2f176ce3151a7f21dcfbb0c090cbfc85a28
Gyeong a Kim <alwayz0121@gmail.com>
2022-08-19 15:49:44 +0900

    #7.7-7.9 Drag + Motion Values

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -28,28 +18,31 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: "100px" },
-};
+//특정 위치로 드래그 되었는지 확인하기 : MotionValues를 이용해 애니메이션 값의 상태와 속도를 추적
+//MotionValue 값은 재랜더링이 안 되도록 React.js에 속해있지 않다.
 
-//드래그 시 특정 테두리 밖을 넘지 못하도록 지정
 function App() {
-  //useRef를 이용해 ref를 만든다.
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const updateScale = useTransform(x, [-700, 0, 700], [2, 1, 0.1]);
+  const rotateZ = useTransform(x, [-700, 700], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-700, 0, 700],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  //실제 x값을 보고 싶다면,
+  useEffect(() => {
+    // x.onChange(() => console.log(x.get()));
+    updateScale.onChange(() => console.log(x.get()));
+  }, [x]);
   return (
-    <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.5} //당겨오는 힘
-          dragConstraints={biggerBoxRef} //biggerBoxRef로 제한하기
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-        />
-      </BiggerBox>
+    <Wrapper style={{ background: gradient }}>
+      {/* <Box style={{ x: x, scale: updateScale }} drag="x" dragSnapToOrigin /> */}
+      <Box style={{ x: x, rotateZ }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
