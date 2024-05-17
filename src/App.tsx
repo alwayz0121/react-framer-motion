c8465c1c33e3ba523be7c3d6e80e6ccf62b0bd2d
alwayz0121 <alwayz0121@gmail.com>
2024-05-17 15:14:11 +0900

    feat: animate(hover, click, toggle)

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,118 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
-import { useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 5px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+const Circle = styled(motion.div)`
+  background-color: #ffffff;
+  height: 40px;
+  width: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  border-radius: 50%;
 `;
 
-//특정 위치로 드래그 되었는지 확인하기 : MotionValues를 이용해 애니메이션 값의 상태와 속도를 추적
-//MotionValue 값은 재랜더링이 안 되도록 React.js에 속해있지 않다.
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const overlay = {
+  hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.2,
+    transition: {
+      type: "tween",
+    },
+  },
+};
 
 function App() {
-  const x = useMotionValue(0);
-  const updateScale = useTransform(x, [-700, 0, 700], [2, 1, 0.1]);
-  const rotateZ = useTransform(x, [-700, 700], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-700, 0, 700],
-    [
-      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
-      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
-      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-    ]
-  );
-  //실제 x값을 보고 싶다면,
-  useEffect(() => {
-    // x.onChange(() => console.log(x.get()));
-    updateScale.onChange(() => console.log(x.get()));
-  }, [x]);
+  const [boxClickId, setBoxClickId] = useState<null | number>(null);
+  const [btnClicked, setBtnClicked] = useState(false);
+  const toggledClicked = () => setBtnClicked((prev) => !prev);
+
   return (
-    <Wrapper style={{ background: gradient }}>
-      {/* <Box style={{ x: x, scale: updateScale }} drag="x" dragSnapToOrigin /> */}
-      <Box style={{ x: x, rotateZ }} drag="x" dragSnapToOrigin />
+    <Wrapper>
+      <Grid>
+        {[1, 2, 3, 4].map((n) =>
+          n === 1 || n === 4 ? (
+            <Box
+              variants={boxVariants}
+              initial="normal"
+              whileHover={"hover"}
+              onClick={() => setBoxClickId(n)}
+              key={n}
+              layoutId={n.toString()}
+              transition={{ type: "tween" }}
+            />
+          ) : n === 2 ? (
+            <Box key={n}>
+              {!btnClicked ? <Circle layoutId="circle" /> : null}
+            </Box>
+          ) : (
+            <Box key={n}>
+              {btnClicked ? <Circle layoutId="circle" /> : null}
+            </Box>
+          )
+        )}
+      </Grid>
+
+      <button onClick={toggledClicked}>Switch</button>
+
+      <AnimatePresence>
+        {boxClickId ? (
+          <Overlay
+            variants={overlay}
+            onClick={() => setBoxClickId(null)}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <Box
+              layoutId={boxClickId.toString()}
+              style={{ background: "rgba(255, 255, 255, 1)" }}
+            />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
